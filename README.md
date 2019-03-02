6dcb58328dd82c391cd2c1a3228d41998e7ad6a1
joyfuI <jong970105@gmail.com>
2019-03-02 10:11:47 +0900

    첫 커밋

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # search-link-fix
+네이버, 구글, 다음 검색엔진에서 사용자 추적을 위해 링크를 변형하는 것을 차단하는 유저 스크립트입니다.
+## 소개
+![네이버](https://i.imgur.com/fQpEsUG.gif)
+![구글](https://i.imgur.com/Sh6ze0Y.gif)
+위 이미지처럼 검색엔진들은 링크를 누르는 순간 추적 링크로 바꾸는 방법을 통해서 사용자가 어떤 링크를 클릭하는지 수집하고 있습니다.  
+이 스크립트는 이런 행위를 차단합니다.
+![적용전후](https://i.imgur.com/SLBMety.png)
+## 설치
+1. 브라우저에 맞게 유저스크립트 확장기능 중 하나를 설치합니다.
+[Greasemonkey](https://www.greasespot.net/) - 파이어폭스  
+[Tampermonkey](https://tampermonkey.net/) - 크롬, 엣지, 사파리, 파이어폭스, 오페라, 돌핀, UC  
+[Violentmonkey](https://violentmonkey.github.io/) - 크롬, 파이어폭스, Maxthon
+2. [여기](https://raw.githubusercontent.com/joyfuI/search-link-fix/master/search-link-fix.user.js)에서 유저 스크립트를 설치합니다.
+3. ????
+4. PROFIT!
+## 주의사항
+검색 페이지에 있는 모든 <a> 태그의 click 이벤트를 무력화 시키는 방식입니다.  
+이로인해 링크를 클릭해도 아무런 반응이 없는 문제가 생길 수 있으므로 문제발견 시 적극적으로 알려주세요.
---
+++ search-link-fix.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name		Search Link Fix
+// @namespace	https://joyfui.wo.tc/
+// @version		1
+// @author		joyfuI
+// @description	네이버, 구글, 다음 검색엔진의 사용자 추적을 위한 링크를 수정합니다.
+// @homepage	https://github.com/joyfuI/search-link-fix
+// @downloadURL	https://raw.githubusercontent.com/joyfuI/search-link-fix/master/search-link-fix.user.js
+// @include		https://www.google.*/search*
+// @include		https://www.google.*.*/search*
+// @include		https://search.naver.com/search.naver*
+// @include		https://search.daum.net/search*
+// @run-at		document-end
+// @grant		none
+// ==/UserScript==
+
+(function() {
+	"use strict";
+	if (location.host.indexOf('google') > -1)	// 구글
+	{
+		var nodes = document.getElementsByTagName('a');
+		for (var i of nodes)
+		{
+			i.removeAttribute('onmousedown');
+			i.removeAttribute('ping');	// 크롬용
+		}
+		var tag = document.createElement('script');
+		tag.textContent = 'var tmp = setInterval(function () {window.rwt = window.s_sFa = function () {};}, 100); setTimeout(function () {clearInterval(tmp);}, 3000);';	// 구글은 이상하게 페이지 로딩 직후엔 적용이 안됨
+		document.head.appendChild(tag);
+	}
+	else if (location.host.indexOf('naver') > -1)	// 네이버
+	{
+		var nodes = document.getElementsByTagName('a');
+		for (var i of nodes)
+			i.removeAttribute('onclick');
+		var tag = document.createElement('script');
+		tag.textContent = 'window.goOtherCR = window.goOtherTCR = function () {};';
+		document.head.appendChild(tag);
+	}
+	else if (location.host.indexOf('daum') > -1)	// 다음
+	{
+		var nodes = document.getElementsByTagName('a');
+		for (var i of nodes)
+			i.removeAttribute('onclick');
+		var tag = document.createElement('script');
+		tag.textContent = 'window.smartLog = function () {};';
+		document.head.appendChild(tag);
+	}
+})();
