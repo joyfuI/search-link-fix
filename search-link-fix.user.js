ee4e2118b86940e46fd3b53fa047627cdc9d3700
joyfuI <jong970105@gmail.com>
2019-03-13 23:15:47 +0900

    모바일 페이지 대응

--- search-link-fix.user.js
+++ search-link-fix.user.js
@@ -1,18 +1,23 @@
 // ==UserScript==
 // @name		Search Link Fix
 // @namespace	https://joyfui.wo.tc/
-// @version		7
+// @version		8
 // @author		joyfuI
 // @description	네이버, 구글, 다음 등 검색엔진의 사용자 추적을 위한 링크를 수정합니다.
-// @homepage	https://github.com/joyfuI/search-link-fix
+// @homepageURL	https://github.com/joyfuI/search-link-fix
 // @downloadURL	https://raw.githubusercontent.com/joyfuI/search-link-fix/master/search-link-fix.user.js
 // @include		https://www.google.*/search*
 // @include		https://www.google.*.*/search*
 // @include		https://search.naver.com/search.naver*
+// @include		https://m.search.naver.com/search.naver*
 // @include		https://search.daum.net/search*
+// @include		https://m.search.daum.net/search*
 // @include		http://search.zum.com/search.zum*
+// @include		https://m.search.zum.com/search.zum*
 // @include		https://search.daum.net/nate*
+// @include		https://m.search.daum.net/nate*
 // @include		http://search.dreamwiz.com/*
+// @include		http://m.search.dreamwiz.com/*
 // @run-at		document-end
 // @grant		unsafeWindow
 // ==/UserScript==
@@ -21,7 +26,7 @@
 	"use strict";
 	if (location.host.indexOf('google') > -1)	// 구글
 	{
-		var nodes = document.getElementById('rcnt').getElementsByTagName('a');
+		var nodes = document.getElementById('main').getElementsByTagName('a');
 		for (var i of nodes)
 		{
 			i.removeAttribute('onmousedown');
@@ -43,9 +48,12 @@
 	}
 	else if (location.host.indexOf('daum') > -1)	// 다음, 네이트
 	{
-		var nodes = document.getElementById('cMain').getElementsByTagName('a');
+		var nodes = document.getElementById('daumContent').getElementsByTagName('a');
 		for (var i of nodes)
+		{
 			i.removeAttribute('onclick');
+			i.removeAttribute('data-docid');
+		}
 		var tag = document.createElement('script');
 		tag.textContent = 'window.smartLog = function () {};';
 		document.head.appendChild(tag);
@@ -61,7 +69,8 @@
 		if (document.getElementById('googleSearchLayer') != null)	// 귀찮게시리 왜 구글 맞춤 검색을 사용하는거야ㅡㅡ
 		{
 			var tmp = setInterval(function () {
-				if (unsafeWindow.googleWebSearch.googleWebSearchTotalPage != 0)	// 구글 맞춤 검색 로딩이 완료되면
+				var googleWebSearch = (typeof(googleWebSearch) === 'undefined') ? unsafeWindow.zum.googleWebSearch : unsafeWindow.googleWebSearch;
+				if (googleWebSearch.googleWebSearchTotalPage != 0)	// 구글 맞춤 검색 로딩이 완료되면
 				{
 					clearInterval(tmp);
 					var nodes = document.getElementById('googleSearchLayer').getElementsByTagName('a');
@@ -76,11 +85,15 @@
 	}
 	else if (location.host.indexOf('dreamwiz') > -1)	// 드림위즈
 	{
-		var nodes = document.getElementsByClassName('midbox_wrap')[0].getElementsByTagName('a');
-		for (var i of nodes)
-			i.removeAttribute('onclick');
+		var nodes = document.getElementsByClassName('midbox_wrap');
+		if (nodes.length !== 0)
+		{
+			nodes = nodes[0].getElementsByTagName('a');
+			for (var i of nodes)
+				i.removeAttribute('onclick');
+		}
 		var tag = document.createElement('script');
-		tag.textContent = 'window.publog_send = function () {};';
+		tag.textContent = 'window.publog_send = window.publog_click = function () {};';
 		document.head.appendChild(tag);
 	}
 })();
