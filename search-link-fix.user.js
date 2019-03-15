0870cd557b20782c42068074d14e266c0812b7cc
joyfuI <jong970105@gmail.com>
2019-03-15 15:03:41 +0900

    코딩 스타일 변경

--- search-link-fix.user.js
+++ search-link-fix.user.js
@@ -22,75 +22,68 @@
 // @grant		unsafeWindow
 // ==/UserScript==
 
-(function() {
+(function () {
 	"use strict";
-	if (location.host.indexOf('google') > -1)	// 구글
-	{
+	if (location.host.indexOf('google') > -1) {	// 구글
 		var nodes = document.getElementById('main').getElementsByTagName('a');
-		for (var i of nodes)
-		{
+		for (var i of nodes) {
 			i.removeAttribute('onmousedown');
 			i.removeAttribute('ping');	// 크롬용
 		}
 		var tag = document.createElement('script');
-		tag.textContent = 'var tmp = setInterval(function () {window.rwt = window.s_sFa = function () {};}, 100); setTimeout(function () {clearInterval(tmp);}, 3000);';	// 이상하게 페이지 로딩 직후엔 적용이 안돼서 타이머 사용
+		tag.textContent = 'var tmp = setInterval(function () { window.rwt = window.s_sFa = function () {}; }, 100); setTimeout(function () { clearInterval(tmp); }, 3000);';	// 이상하게 페이지 로딩 직후엔 적용이 안돼서 타이머 사용
 		document.head.appendChild(tag);
-	}
-	else if (location.host.indexOf('naver') > -1)	// 네이버
-	{
+
+	} else if (location.host.indexOf('naver') > -1) {	// 네이버
 		var nodes = document.getElementById('container').getElementsByTagName('a');
-		for (var i of nodes)
-			if (i.href.charAt(i.href.length - 1) != '#')	// # 링크 제외
+		for (var i of nodes) {
+			if (i.href.charAt(i.href.length - 1) != '#') {	// # 링크 제외
 				i.removeAttribute('onclick');
+			}
+		}
 		var tag = document.createElement('script');
 		tag.textContent = 'window.goOtherCR = window.goOtherTCR = function () {};';
 		document.head.appendChild(tag);
-	}
-	else if (location.host.indexOf('daum') > -1)	// 다음, 네이트
-	{
+
+	} else if (location.host.indexOf('daum') > -1) {	// 다음, 네이트
 		var nodes = document.getElementById('daumContent').getElementsByTagName('a');
-		for (var i of nodes)
-		{
+		for (var i of nodes) {
 			i.removeAttribute('onclick');
 			i.removeAttribute('data-docid');
 		}
 		var tag = document.createElement('script');
 		tag.textContent = 'window.smartLog = function () {};';
 		document.head.appendChild(tag);
-	}
-	else if (location.host.indexOf('zum') > -1)	// 줌
-	{
+
+	} else if (location.host.indexOf('zum') > -1) {	// 줌
 		var nodes = document.getElementById('container').getElementsByTagName('a');
-		for (var i of nodes)
+		for (var i of nodes) {
 			i.removeAttribute('onclick');
+		}
 		var tag = document.createElement('script');
-		tag.textContent = 'var tmp = setInterval(function () {window.goOutlink = window.zum.a.D = function () {};}, 100); setTimeout(function () {clearInterval(tmp);}, 3000);';	// 이상하게 페이지 로딩 직후엔 적용이 안돼서 타이머 사용
+		tag.textContent = 'var tmp = setInterval(function () { window.goOutlink = window.zum.a.D = function () {}; }, 100); setTimeout(function () { clearInterval(tmp); }, 3000);';	// 이상하게 페이지 로딩 직후엔 적용이 안돼서 타이머 사용
 		document.head.appendChild(tag);
-		if (document.getElementById('googleSearchLayer') != null)	// 귀찮게시리 왜 구글 맞춤 검색을 사용하는거야ㅡㅡ
-		{
+		if (document.getElementById('googleSearchLayer') != null) {	// 귀찮게시리 왜 구글 맞춤 검색을 사용하는거야ㅡㅡ
 			var tmp = setInterval(function () {
 				var googleWebSearch = (typeof(googleWebSearch) === 'undefined') ? unsafeWindow.zum.googleWebSearch : unsafeWindow.googleWebSearch;
-				if (googleWebSearch.googleWebSearchTotalPage != 0)	// 구글 맞춤 검색 로딩이 완료되면
-				{
+				if (googleWebSearch.googleWebSearchTotalPage != 0) {	// 구글 맞춤 검색 로딩이 완료되면
 					clearInterval(tmp);
 					var nodes = document.getElementById('googleSearchLayer').getElementsByTagName('a');
-					for (var i of nodes)
-					{
+					for (var i of nodes) {
 						i.removeAttribute('data-cturl');
 						i.removeAttribute('data-ctorig');
 					}
 				}
 			}, 100);
 		}
-	}
-	else if (location.host.indexOf('dreamwiz') > -1)	// 드림위즈
-	{
+
+	} else if (location.host.indexOf('dreamwiz') > -1) {	// 드림위즈
 		var nodes = document.getElementsByClassName('midbox_wrap');
-		if (nodes.length !== 0)
-		{
+		if (nodes.length !== 0) {
 			nodes = nodes[0].getElementsByTagName('a');
-			for (var i of nodes)
+			for (var i of nodes) {
 				i.removeAttribute('onclick');
+			}
 		}
 		var tag = document.createElement('script');
 		tag.textContent = 'window.publog_send = window.publog_click = function () {};';
