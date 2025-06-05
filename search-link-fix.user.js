fd8effda07306f98d77a4aeaa677f98390328ce2
joyfuI <1342862+joyfuI@users.noreply.github.com>
2025-06-06 00:50:02 +0900

    코드 리팩토링

--- search-link-fix.user.js
+++ search-link-fix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Search Link Fix
 // @namespace    https://blog.joyfui.com/
-// @version      10
+// @version      11
 // @author       joyfuI
 // @description  네이버, 구글, 다음 등 검색엔진의 사용자 추적을 위한 링크를 수정합니다.
 // @homepageURL  https://github.com/joyfuI/search-link-fix
@@ -17,35 +17,64 @@
 // @include      https://search.zum.com/search.zum*
 // @include      https://m.search.zum.com/search.zum*
 // @run-at       document-end
+// @grant        none
 // ==/UserScript==
 
 (() => {
   "use strict";
   const domain = location.host.split('.');
 
+  const processLinks = (container, selector = 'a') => {
+    if (!container) {
+      return;
+    }
+    const elements = container.querySelectorAll(selector);
+    for (let element of elements) {
+      element.removeAttribute('onclick');
+      element.removeAttribute('ping');  // 크롬용(구글검색)
+      for (let key of Object.keys(element.dataset)) {
+        delete element.dataset[key];
+      }
+    }
+  }
+
+  // 구글 맞춤 검색처럼 동적으로 추가된 링크도 처리하기 위해 MutationObserver 사용
+  const setupObserver = (container, selector = 'a') => {
+    if (!container) {
+      return;
+    }
+    const observer = new MutationObserver((mutations) => {
+      for (let mutation of mutations) {
+        if (mutation.addedNodes.length) {
+          for (let node of mutation.addedNodes) {
+            if (node.nodeType === Node.ELEMENT_NODE) {
+              processLinks(node, selector);
+            }
+          }
+        }
+      }
+    });
+    observer.observe(container, {
+      childList: true,
+      subtree: true
+    });
+    return observer;
+  }
+
   // 구글
   if (domain.includes('google')) {
-    const elements = document.getElementById('res').getElementsByTagName('a');
-    for (let element of elements) {
-      element.removeAttribute('ping');  // 크롬용
-      Object.keys(element.dataset).forEach((key) => {
-        delete element.dataset[key];
-      });
-    }
+    const container = document.getElementById('res');
+    const selector = 'a';
+    processLinks(container, selector);
+    setupObserver(container, selector);
   }
 
   // 네이버
   else if (domain.includes('naver')) {
-    const elements = document.getElementById('container').getElementsByTagName('a');
-    for (let element of elements) {
-      if (element.href === '#') {
-        break;  // # 링크 제외
-      }
-      element.removeAttribute('onclick');
-      Object.keys(element.dataset).forEach((key) => {
-        delete element.dataset[key];
-      });
-    }
+    const container = document.getElementById('container');
+    const selector = 'a:not([href="#"])';
+    processLinks(container, selector);
+    setupObserver(container, selector);
     const script = document.createElement('script');
     script.textContent = 'window.goOtherCR = function () {};';
     document.head.appendChild(script);
@@ -53,12 +82,10 @@
 
   // 다음, 네이트
   else if (domain.includes('daum')) {
-    const elements = document.getElementById('daumContent').getElementsByTagName('a');
-    for (let element of elements) {
-      Object.keys(element.dataset).forEach((key) => {
-        delete element.dataset[key];
-      });
-    }
+    const container = document.getElementById('daumContent');
+    const selector = 'a:not([href="javascript:"])';
+    processLinks(container, selector);
+    setupObserver(container, selector);
     const script = document.createElement('script');
     script.textContent = 'window.smartLog = function () {};';
     document.head.appendChild(script);
@@ -66,27 +93,9 @@
 
   // 줌
   else if (domain.includes('zum')) {
-    const elements = document.getElementById('container').getElementsByTagName('a');
-    for (let element of elements) {
-      element.removeAttribute('onclick');
-      Object.keys(element.dataset).forEach((key) => {
-        delete element.dataset[key];
-      });
-    }
-    if (document.getElementById('googleSearchLayer') !== null) {
-      // 귀찮게시리 왜 구글 맞춤 검색을 사용하는거야ㅡㅡ
-      const tmp = setInterval(() => {
-        if (document.getElementById('googleLoadingImg').style.display === 'none') {
-          // 구글 맞춤 검색 로딩이 완료되면
-          clearInterval(tmp);
-          const elements = document.getElementById('googleSearchLayer').getElementsByTagName('a');
-          for (let element of elements) {
-            Object.keys(element.dataset).forEach((key) => {
-              delete element.dataset[key];
-            });
-          }
-        }
-      }, 100);
-    }
+    const container = document.getElementById('container');
+    const selector = 'a';
+    processLinks(container, selector);
+    setupObserver(container, selector);
   }
 })();
