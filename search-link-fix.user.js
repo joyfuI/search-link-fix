79e123ad04c6ee6a680a64ed5a61b09da1213ded
joyfuI <1342862+joyfuI@users.noreply.github.com>
2025-06-05 21:35:41 +0900

    주석 수정

--- search-link-fix.user.js
+++ search-link-fix.user.js
@@ -1,22 +1,22 @@
 // ==UserScript==
-// @name				Search Link Fix
-// @namespace		https://blog.joyfui.com/
-// @version			10
-// @author			joyfuI
-// @description	네이버, 구글, 다음 등 검색엔진의 사용자 추적을 위한 링크를 수정합니다.
-// @homepageURL	https://github.com/joyfuI/search-link-fix
-// @downloadURL	https://raw.githubusercontent.com/joyfuI/search-link-fix/master/search-link-fix.user.js
-// @include			https://www.google.*/search*
-// @include			https://www.google.*.*/search*
-// @include			https://search.naver.com/search.naver*
-// @include			https://m.search.naver.com/search.naver*
-// @include			https://search.daum.net/search*
-// @include			https://m.search.daum.net/search*
-// @include			https://search.daum.net/nate*
-// @include			https://m.search.daum.net/nate*
-// @include			https://search.zum.com/search.zum*
-// @include			https://m.search.zum.com/search.zum*
-// @run-at			document-end
+// @name         Search Link Fix
+// @namespace    https://blog.joyfui.com/
+// @version      10
+// @author       joyfuI
+// @description  네이버, 구글, 다음 등 검색엔진의 사용자 추적을 위한 링크를 수정합니다.
+// @homepageURL  https://github.com/joyfuI/search-link-fix
+// @downloadURL  https://raw.githubusercontent.com/joyfuI/search-link-fix/master/search-link-fix.user.js
+// @include      https://www.google.*/search*
+// @include      https://www.google.*.*/search*
+// @include      https://search.naver.com/search.naver*
+// @include      https://m.search.naver.com/search.naver*
+// @include      https://search.daum.net/search*
+// @include      https://m.search.daum.net/search*
+// @include      https://search.daum.net/nate*
+// @include      https://m.search.daum.net/nate*
+// @include      https://search.zum.com/search.zum*
+// @include      https://m.search.zum.com/search.zum*
+// @run-at       document-end
 // ==/UserScript==
 
 (() => {
@@ -73,9 +73,11 @@
         delete element.dataset[key];
       });
     }
-    if (document.getElementById('googleSearchLayer') !== null) { // 귀찮게시리 왜 구글 맞춤 검색을 사용하는거야ㅡㅡ
+    if (document.getElementById('googleSearchLayer') !== null) {
+      // 귀찮게시리 왜 구글 맞춤 검색을 사용하는거야ㅡㅡ
       const tmp = setInterval(() => {
-        if (document.getElementById('googleLoadingImg').style.display === 'none') {  // 구글 맞춤 검색 로딩이 완료되면
+        if (document.getElementById('googleLoadingImg').style.display === 'none') {
+          // 구글 맞춤 검색 로딩이 완료되면
           clearInterval(tmp);
           const elements = document.getElementById('googleSearchLayer').getElementsByTagName('a');
           for (let element of elements) {
